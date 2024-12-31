aec5045bdfdcf652bd6d6cc574a85d4ca86d86cd
JesusRodrigoV <jaicelvelasco105@gmail.com>
2024-12-30 21:59:44 -0400

    Creadas clase Laboratorio

--- src/main/java/com/farmacia/gestion/model/Estado.java
+++ src/main/java/com/farmacia/gestion/model/Estado.java
@@ -20,23 +20,40 @@ public class Estado {
     @Column(name = "descripcion", nullable = false, length = 15)
     private Tipo descripcion;
 
-    public Estado(Tipo descripcion){
+    public Estado(Tipo descripcion) {
         this.descripcion = descripcion;
     }
-    
-    public Estado(){}
+
+    public Estado() {
+    }
 
     public void setDescripcion(Tipo descripcion) {
         this.descripcion = descripcion;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public Tipo getDescripcion() {
         return descripcion;
     }
+
     public Long getId() {
         return id;
     }
 
+    /*
+     * 
+     * 
+     * public void desactivarProducto(Long productoId) {
+     * Producto producto = productoRepository.findById(productoId).orElseThrow(() ->
+     * new NotFoundException("Producto no encontrado"));
+     * Estado estadoInactivo =
+     * estadoRepository.findByDescripcion(EstadoTipo.INACTIVO);
+     * producto.setEstado(estadoInactivo);
+     * productoRepository.save(producto);
+     * }
+     * 
+     */
 }
---
+++ src/main/java/com/farmacia/gestion/model/Laboratorio.java
@@ -0,0 +1,91 @@
+package com.farmacia.gestion.model;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "laboratorios")
+public class Laboratorio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_laboratorio")
+    private Long id;
+
+    @Column(name = "nombre", nullable = false, length = 50)
+    private String nombre;
+
+    @Column(name = "telefono", nullable = false, length = 20)
+    private String telefono;
+
+    @Column(name = "email", nullable = false, length = 30)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "id_estado")
+    @Column(name = "id_estado")
+    private Estado estado;
+
+    @Column(name = "fecha_registro", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Timestamp fecha_registro;
+
+    public Laboratorio(String nombre, String telefono, String email, Estado estado, Timestamp fecha_registro) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+        this.estado = estado;
+        this.fecha_registro = fecha_registro;
+    }
+
+    public Laboratorio() {
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public void setFecha_registro(Timestamp fecha_registro) {
+        this.fecha_registro = fecha_registro;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public Timestamp getFecha_registro() {
+        return fecha_registro;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+}
